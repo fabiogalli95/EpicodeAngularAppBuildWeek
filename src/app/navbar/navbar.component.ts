b9d992bf6db9ae0720240e51967f56518636fb37
gianluca <gianluca.giacchetta@gmail.com>
2024-04-09 14:13:58 +0200

    upd gianluca

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,8 +1,4 @@
 import { Component } from '@angular/core';
-
-
-
-
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -13,6 +9,7 @@ export class NavbarComponent {
    userJson = localStorage.getItem('user');
   user = this.userJson ? JSON.parse(this.userJson) : null;
   constructor(){
+
 
   }
 
